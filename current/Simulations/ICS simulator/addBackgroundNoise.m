b948d810833666b70b95c5fd7bb1fb6ac8f0da05
Rodrigo Migueles R <55300581+Malvick17@users.noreply.github.com>
2022-07-31 11:47:09 -0400

    Re-organize repo to the 2006 version

--- current/Simulations/ICS simulator/addBackgroundNoise.m
+++
@@ -1,7 +0,0 @@
-function noiseMatrix=addBackgroundNoise(matrix, coefficient)
-
-% adds random numbers normally distributed with variance coefficeint*mean(image)
-% to an image matrix
-
-noiseMatrix=abs(randn(size(matrix)));
-noiseMatrix=matrix+coefficient*max(nonzeros(matrix))*noiseMatrix;
--- current/Simulations/Photobleach simulator/createImage.m
+++
@@ -1,8 +0,0 @@
-function imageMatrix=createImage(sizeImage, numParticles)
-
-% creates a matrix with numParticles number of particles 
-
-xCoor=ceil(sizeImage*rand(numParticles, 1));
-yCoor=ceil(sizeImage*rand(numParticles, 1));
-
-imageMatrix=full(sparse(xCoor, yCoor, 1, sizeImage, sizeImage));
